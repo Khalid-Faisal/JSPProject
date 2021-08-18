c01d1165483e13ad102c811fb696f47f2408d8c4
Khalid Faisal <khalid.datamax@gmail.com>
2021-08-19 04:21:51 +0530

    Arithmetic Operations By first

---
+++ first/Add.java
@@ -0,0 +1,7 @@
+public class Add{
+	public static void main(String args[]){
+		int a = 24;
+		int b = 27;
+		System.out.println("Addition is : "+(a+b)); 
+	}
+}
---
+++ first/Difference.java
@@ -0,0 +1,7 @@
+public class Difference{
+	public static void main(String args[]){
+		int a = 24;
+		int b = 21;
+		System.out.println("Difference is : "+(a-b)); 
+	}
+}
